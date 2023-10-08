3d10c046dc49a37b890be360298652d1180098a3
caua-3301 <134548536+caua-3301@users.noreply.github.com>
2023-10-07 23:27:38 -0300

    Criando cards de solicitaco

---
+++ homepage/js/script.js
@@ -0,0 +1,28 @@
+//Sistema responsável por exibir as solicitações ao funcionário
+
+let bancoDados = [{
+    nome: "Cauã Pires Soares",
+    cpf: "113.424.543-77",
+    descricao: "Remoção de árvore na rua almeida",
+    data: "11/10/2023"
+},
+
+{
+    nome: "Riquelme Oliveira",
+    cpf: "141.332.613-17",
+    descricao: "Consertar vazamento da água da cagepa, rua belmonte",
+    data: "01/09/2023"
+},
+
+{
+    nome: "Daniel Macho",
+    cpf: "242.242.242-24",
+    descricao: "Remoção de árvore na rua almeida",
+    data: "11/10/2023"
+},
+{
+    nome: "Cauã Pires Soares",
+    cpf: "113.424.543-77",
+    descricao: "Remoção de árvore na rua almeida",
+    data: "11/10/2023"
+}]
